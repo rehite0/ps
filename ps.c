bfa343c3fed3bb5baf31516b33b8404dc6983585
rh0 <125304115+rehite1@users.noreply.github.com>
2024-10-31 18:30:51 +0530

    sync small changes

--- ps.c
+++ ps.c
@@ -237,7 +237,7 @@ model_setup(){
 	a->pos[0]=0.0;		a->pos[1]=0.0;
 	a->ppos[0]=0.0;		a->ppos[1]=0.0;
 	a->acc[0]=0.0;		a->acc[1]=0.0;
-	a->color[0]=0.5;	a->color[1]=0.0;	a->color[2]=0.0;	a->color[3]=1.0;
+	a->color[0]=1.0;	a->color[1]=1.0;	a->color[2]=0.0;	a->color[3]=1.0;
 	a->rad=0.03;		a->flag=NO_FORCE|NO_CONSTRAIN|NO_MOVE|NO_COLLISION|NO_DISPLAY;
 	ball_buff[BALL_COUNT-1]=a;
 	mouse_ball=a;
