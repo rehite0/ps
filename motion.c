4ea0689c01d99571675a9ff97e0f4632025d5606
rh0 <125304115+rehite1@users.noreply.github.com>
2025-06-13 19:44:59 +0530

    add collision cofficient for wall

--- motion.c
+++ motion.c
@@ -40,22 +40,27 @@ void apply_constrains(BALL start,BALL stop,const float dt){
 	for (BALL i=start;i<stop;++i){
 		if ((ball_buff.flag[i]&NO_CONSTRAIN)==NO_CONSTRAIN) continue;
 		float wall=1.0f-ball_buff.rad[i];
+		const float coll_coff=0.8f;
 		if (ball_buff.posx[i]>=wall){
 			ball_buff.posx[i]=2.0f*wall-ball_buff.posx[i];
 			ball_buff.pposx[i]=2.0f*wall-ball_buff.pposx[i];
+			ball_buff.pposx[i]=ball_buff.posx[i]*(1.0f-coll_coff)+ball_buff.pposx[i]*coll_coff;
 		}
 		if (ball_buff.posy[i]>=wall){
 			ball_buff.posy[i]=2.0f*wall-ball_buff.posy[i];
 			ball_buff.pposy[i]=2.0f*wall-ball_buff.pposy[i];
+			ball_buff.pposy[i]=ball_buff.posy[i]*(1.0f-coll_coff)+ball_buff.pposy[i]*coll_coff;
 		}
 		wall*=-1.0f;
 		if (ball_buff.posx[i]<=wall){
 			ball_buff.posx[i]=2.0f*wall-ball_buff.posx[i];
 			ball_buff.pposx[i]=2.0f*wall-ball_buff.pposx[i];
+			ball_buff.pposx[i]=ball_buff.posx[i]*(1.0f-coll_coff)+ball_buff.pposx[i]*coll_coff;
 		}
 		if (ball_buff.posy[i]<=wall){
 			ball_buff.posy[i]=2.0f*wall-ball_buff.posy[i];
 			ball_buff.pposy[i]=2.0f*wall-ball_buff.pposy[i];
+			ball_buff.pposy[i]=ball_buff.posy[i]*(1.0f-coll_coff)+ball_buff.pposy[i]*coll_coff;
 		}
 	}
 }
